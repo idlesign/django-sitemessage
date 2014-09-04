19370e6b3cf32906fb24b7c52f70f3a132730847
idle sign <idlesign@yandex.ru>
2014-09-04 18:53:44 +0700

    runtests adapted for Django 1.7

--- sitemessage/runtests.py
+++ sitemessage/runtests.py
@@ -2,19 +2,26 @@
 import sys
 import os
 
-from django.conf import settings
+from django.conf import settings, global_settings
 
 
 def main():
-    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-    app_name = 'sitemessage'
+    app_name = os.path.dirname(__file__)
 
     if not settings.configured:
         settings.configure(
             INSTALLED_APPS=('django.contrib.auth', 'django.contrib.contenttypes', app_name),
             DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3'}},
+            MIDDLEWARE_CLASSES=global_settings.MIDDLEWARE_CLASSES,  # Prevents Django 1.7 warning.
         )
+
+    try:  # Django 1.7 +
+        from django import setup
+        setup()
+    except ImportError:
+        pass
 
     from django.test.utils import get_runner
     runner = get_runner(settings)()
