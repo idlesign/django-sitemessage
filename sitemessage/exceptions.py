989feacc8027cae52b61983ddc40cfbb0be17a0e
idle sign <idlesign@yandex.ru>
2015-10-31 00:16:46 +0600

    Added MessengerException class.

--- sitemessage/exceptions.py
+++ sitemessage/exceptions.py
@@ -15,5 +15,9 @@ class UnknownMessengerError(SiteMessageError):
     """This error is raised when there's a try to access an unknown messenger."""
 
 
-class MessengerWarmupException(SiteMessageError):
+class MessengerException(SiteMessageError):
+    """Base messenger exception."""
+
+
+class MessengerWarmupException(MessengerException):
     """This exception represents a delivery error due to a messenger warm up process failure."""
