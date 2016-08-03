8e6897b650bd86d586bcb890257328aa3a79bebf
idle sign <idlesign@yandex.ru>
2016-08-03 10:04:41 +0700

    Django 1.10 compatibility improved.

--- sitemessage/management/commands/sitemessage_send_scheduled.py
+++ sitemessage/management/commands/sitemessage_send_scheduled.py
@@ -1,18 +1,25 @@
-from optparse import make_option
-
 from django.core.management.base import BaseCommand
 
 from sitemessage.toolbox import send_scheduled_messages
+from sitemessage.compat import CommandOption, options_getter
+
+
+get_options = options_getter((
+    CommandOption(
+        '--priority', action='store', dest='priority', default=None,
+        help='Allows to filter scheduled messages by a priority number. Defaults to None.'
+    ),
+))
 
 
 class Command(BaseCommand):
 
     help = 'Sends scheduled messages (both in pending and error statuses).'
 
-    option_list = BaseCommand.option_list + (
-        make_option('--priority', action='store', dest='priority', default=None,
-                    help='Allows to filter scheduled messages by a priority number. Defaults to None.'),
-    )
+    option_list = get_options()
+
+    def add_arguments(self, parser):
+        get_options(parser.add_argument)
 
     def handle(self, *args, **options):
         priority = options.get('priority', None)
--- sitemessage/runtests.py
+++ sitemessage/runtests.py
@@ -11,13 +11,30 @@ def main():
     sys.path.insert(0, os.path.join(current_dir, '..'))
 
     if not settings.configured:
-        settings.configure(
+        configure_kwargs = dict(
             INSTALLED_APPS=('django.contrib.auth', 'django.contrib.contenttypes', app_name),
             DATABASES={'default': {'ENGINE': 'django.db.backends.sqlite3'}},
             MIDDLEWARE_CLASSES=global_settings.MIDDLEWARE_CLASSES,  # Prevents Django 1.7 warning.
             ROOT_URLCONF='sitemessage.tests',
             STATIC_URL='static/'
         )
+
+        try:
+            configure_kwargs['TEMPLATE_CONTEXT_PROCESSORS'] = tuple(global_settings.TEMPLATE_CONTEXT_PROCESSORS) + (
+                'django.core.context_processors.request',
+            )
+
+        except AttributeError:
+
+            # Django 1.8+
+            configure_kwargs['TEMPLATES'] = [
+                {
+                    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                    'APP_DIRS': True,
+                },
+            ]
+
+        settings.configure(**configure_kwargs)
 
     try:  # Django 1.7 +
         from django import setup
