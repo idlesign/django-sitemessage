2198eddd40ee249cc5fee67881fd919f645ec6d6
idle sign <idlesign@yandex.ru>
2020-10-31 09:09:28 +0700

    Fixed deprecation warning

--- sitemessage/signals.py
+++ sitemessage/signals.py
@@ -2,14 +2,26 @@
 import django.dispatch
 
 
-# Emitted when user unsubscribe requested is successful.
-sig_unsubscribe_success = django.dispatch.Signal(providing_args=['request', 'message', 'dispatch'])
+sig_unsubscribe_success = django.dispatch.Signal()
+"""Emitted when user unsubscribe requested is successful.
+providing_args=['request', 'message', 'dispatch']
 
-# Emitted when user unsubscribe requested fails.
-sig_unsubscribe_failed = django.dispatch.Signal(providing_args=['request', 'message', 'dispatch'])
+"""
 
-# Emitted when mark read requested is successful.
-sig_mark_read_success = django.dispatch.Signal(providing_args=['request', 'message', 'dispatch'])
+sig_unsubscribe_failed = django.dispatch.Signal()
+"""Emitted when user unsubscribe requested fails.
+providing_args=['request', 'message', 'dispatch']
 
-# Emitted when mark read requested fails.
-sig_mark_read_failed = django.dispatch.Signal(providing_args=['request', 'message', 'dispatch'])
+"""
+
+sig_mark_read_success = django.dispatch.Signal()
+"""Emitted when mark read requested is successful.
+providing_args=['request', 'message', 'dispatch']
+
+"""
+
+sig_mark_read_failed = django.dispatch.Signal()
+"""Emitted when mark read requested fails.
+providing_args=['request', 'message', 'dispatch']
+
+"""
