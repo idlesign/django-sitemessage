a2db821f9365194cef5e120251f8efa476a6b2af
idle sign <idlesign@yandex.ru>
2021-09-09 19:31:38 +0700

    Improved admin pages performance for Subscription, Dispatch and Message (raw_id_fields)

--- sitemessage/admin.py
+++ sitemessage/admin.py
@@ -23,6 +23,7 @@ class MessageAdmin(admin.ModelAdmin):
 
     list_display = ('time_created', 'cls', 'dispatches_ready')
     list_filter = ('cls', 'dispatches_ready')
+    raw_id_fields = ('sender',)
     ordering = ('-time_created',)
 
     inlines = (DispatchInlineAdmin,)
@@ -33,7 +34,7 @@ class DispatchAdmin(admin.ModelAdmin):
     list_display = ('time_created', 'dispatch_status', 'address', 'time_dispatched', 'messenger', 'retry_count')
     list_filter = ('dispatch_status', 'messenger')
     ordering = ('-time_created',)
-    raw_id_fields = ('recipient',)
+    raw_id_fields = ('recipient', 'message')
     readonly_fields = ('retry_count',)
 
     actions = [
@@ -60,6 +61,7 @@ class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('time_created', 'message_cls', 'messenger_cls')
     ordering = ('-time_created',)
     readonly_fields = ('time_created',)
+    raw_id_fields = ('recipient',)
     list_filter = ('message_cls', 'messenger_cls')
 
 
